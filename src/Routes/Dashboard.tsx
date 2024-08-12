d1ea273228b256e36faad7a024860df19ba8c72b
larry king <larryking8118@gmail.com>
2024-08-12 17:40:27 +0000

    removing abort controller from dashboard contents load

--- src/Routes/Dashboard.tsx
+++ src/Routes/Dashboard.tsx
@@ -66,7 +66,7 @@ const Dashboard = ( ) => {
             }
             catch( error ) {
                 console.log( error )
-                HandleOpenErrorDialog()
+                // HandleOpenErrorDialog()
             }
         }
         FetchAllStaff()
